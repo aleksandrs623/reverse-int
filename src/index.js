d82d741c3951637af8be12447a181ccd9d9e342a
Aleksandrs <alexandr.cursinov@gmail.com>
2023-02-14 14:00:05 +0200

    feat: add reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+    let str = String(n);
+    let reverse = str.split('').reverse().join('');
+    if (str[0] == "-") {
+        let cut = str.substring(1);
+        return cut.split('').reverse().join('');
+    } else
+    return reverse;
 }
+
+
+
